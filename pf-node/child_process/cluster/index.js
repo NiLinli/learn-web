01c5bc4d20000cf080d6ab6cfaea0f9fe42dbfa4
nilinli <nilinli@tuhu.cn>
2020-11-18 11:11:41 +0800

    [pf-node] 改变 cluster 的写法

--- pf-node/child_process/cluster/index.js
+++
@@ -1,6 +0,0 @@
-const cluster = require('cluster');
-const master = require('./master');
-const work = require('./work');
-
-cluster.isMaster ? master() : work();
-// cluster.isWorker
--- pf-node/child_process/cluster/work.js
+++ pf-node/child_process/cluster/work.js
@@ -1,17 +1,15 @@
 const http = require('http');
-const cluster = require('cluster');
 
 // cluster.work 当前 work 进程
-module.exports = () => {
-  http
-    .createServer((req, res) => {
-      res.writeHead(200);
-      res.end('hello world\n');
-      process.send({ cmd: 'notifyRequest' });
 
-      omg.a;  // crash code
-    })
-    .listen(8008);
+http
+  .createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+    process.send({ cmd: 'notifyRequest' });
 
-  console.log(`work 进程 ${process.pid} 已启动`);
-};
+    omg.a; // crash code
+  })
+  .listen(8008);
+
+console.log(`work 进程 ${process.pid} 已启动`);
