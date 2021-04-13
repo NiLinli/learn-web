149539c8b37e0e7ae52035bee4ca5ff34a5b6bd1
nilinli <nilinli@tuhu.cn>
2021-04-13 14:25:40 +0800

    [data-structure] graph

---
+++ data-structure/graph/graph-bfs/index.js
@@ -0,0 +1,48 @@
+// 通过散列表记录图
+// 记录当前节点与其邻居的关系
+
+const graph = {};
+
+graph['you'] = ['alice', 'bob', 'claire'];
+graph['bob'] = ['anuj', 'peggy'];
+graph['alice'] = ['peggy'];
+graph['claire'] = ['thom', 'jonny'];
+graph['anuj'] = [];
+graph['peggy'] = [];
+graph['thom'] = [];
+graph['jonny'] = [];
+
+const graph2 = {};
+graph2['you'] = ['mike'];
+graph2['mike'] = ['you'];
+
+function bfsSearch(graph) {
+  let queue = [];
+  let searchRecord = [];
+
+  queue = queue.concat(graph['you']);
+
+  while (queue.length > 0) {
+    // dequeue
+    const pick = queue.shift();
+    // 检查过的不再检查
+    if (searchRecord.includes(pick)) continue;
+
+    if (isMongoDms(pick)) {
+      return pick;
+    } else {
+      searchRecord.push(pick);
+      queue = queue.concat(graph[pick]);
+    }
+  }
+
+  // 没找到符合条件的
+  return false;
+}
+
+function isMongoDms(name) {
+  return name[name.length - 1] === 'm';
+}
+
+console.log(bfsSearch(graph));
+console.log(bfsSearch(graph2));
