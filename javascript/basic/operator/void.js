ba345ebf5b2090b7d5ebfe6ffc9734ad26a4074d
nilinli <nilinli@tuhu.cn>
2021-04-22 18:48:14 +0800

    [javascript] void 0

--- javascript/basic/operator/void.js
+++ javascript/basic/operator/void.js
@@ -1,17 +1,24 @@
-// void
-// 1. 求出 操作数(表达式)的 值
-// 2. 忽视这个值
-// 3. return undefined
-
-// 作用：
-// 希望运行表达式, 但是消除表达式的 side effect(不要表达式的返回值)
+// void 干巴巴执行 expression, 消除表达式的 side effect
+// 1. 执行 expression
+// 2. 抛弃 expression 返回的结果, 直接返回 undefined
 
 // 不希望 doSomething 的返回值赋值给 href
 // <a href="javascript:void doSomething()"></a>
 
 // arrow function 中避免产生副作用
-[1,2,3].forEach(v => void foo(v))
+[1, 2, 3].forEach((v) => void foo(v));
 
 function foo(a) {
   console.log(a + 1);
 }
+
+function rewriteUndefined() {
+  // local scope 可以使用 undefined 做变量名
+  var undefined = 3;
+  var a;
+
+  console.log(a === undefined);
+  console.log(a === void 0);
+}
+
+rewriteUndefined();
