1168c4084d7377f64b7f7c602bc706db44e89278
nilinli <nilinli@tuhu.cn>
2021-04-23 15:10:56 +0800

    [c] bitwise

--- c/language-basic/operator/bitwise/left-shift.c
+++ c/language-basic/operator/bitwise/left-shift.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 // 左移 在C 语言中默认是逻辑左移
+// **有符号数字左移无法保证符号位**
+// 正数/无符号数 左移动 * 2^n 
 int main(int argc, char const *argv[]) {
 
   unsigned int b = 0x00000002;
@@ -15,9 +17,13 @@ int main(int argc, char const *argv[]) {
   printf("c = 0x%x, sizeof(c) = %lu\n", c, sizeof(c));
   printf("c<<2 = 0x%x, sizeof(c << 2) = %lu\n", c << 2, sizeof(c << 2));
 
-  int d = 0xFFFFFFF9; // -6
+  int d = 0xFFFFFFF9; // -7
   printf("d = 0x%x, d = %d, sizeof(d) = %lu\n", d, d,sizeof(d));
   printf("d<<2 = 0x%x, d<<2 = %d, sizeof(d << 2) = %lu\n", d << 2,d << 2, sizeof(d << 2));
+
+  int e = 0x80000009; 
+  printf("e = 0x%x, e = %d, sizeof(e) = %lu\n", e, e,sizeof(e));
+  printf("e<<2 = 0x%x, e<<2 = %d, sizeof(e << 2) = %lu\n", e << 2,e << 2, sizeof(e << 2));
 
   return 0;
 }
--- c/language-basic/operator/bitwise/right-shift.c
+++ c/language-basic/operator/bitwise/right-shift.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  // 有符号: 算术右移
+  // 有符号: 算术右移 >>
   int a = 0x80000000;
   // 1000 0000 0000 0000 0000 0000 0000 0000
   // 1110 0000 0000 0000 0000 0000 0000 0000
-  printf("a = 0x%x, a = %d, sizeof(a) = %lu\n", a,a, sizeof(a));
-  printf("a>>2 = 0x%x, a>>2 = %d, sizeof(a >> 2) = %lu\n", a >> 2, a >> 2, sizeof(a << 2));
+  printf("a = 0x%x, a = %d, sizeof(a) = %lu\n", a, a, sizeof(a));
+  printf("a>>2 = 0x%x, a>>2 = %d, sizeof(a >> 2) = %lu\n\n", a >> 2, a >> 2,
+         sizeof(a << 2));
 
-  // 无符号: 逻辑右移
+  // 无符号: 逻辑右移 >>>
   unsigned int b = 0x80000000;
-  printf("b = 0x%x, sizeof(b) = %lu\n", b, sizeof(b));
-  printf("b>>2 = 0x%x, sizeof(b >> 2) = %lu\n", b >> 2, sizeof(b >> 2));
+  printf("b = 0x%x, b = %u, sizeof(b) = %lu\n", b, b, sizeof(b));
+  printf("b>>2 = 0x%x, b>>2 = %u, sizeof(b >> 2) = %lu\n\n", b >> 2, b >> 2,
+         sizeof(b >> 2));
 
   unsigned int c = 125;
   // .... 0111 1101
   // .... 0001 1111
-  printf("c = 0x%x, a = %d, sizeof(c) = %lu\n", c, c, sizeof(c));
-  printf("c>>2 = 0x%x, a>>2 = %d, sizeof(c >> 2) = %lu\n", c >> 2, c >> 2, sizeof(c >> 2));
+  printf("c = 0x%x, c = %d, sizeof(c) = %lu\n", c, c, sizeof(c));
+  printf("c>>2 = 0x%x, c>>2 = %d, sizeof(c >> 2) = %lu\n\n", c >> 2, c >> 2,
+         sizeof(c >> 2));
 
   // 将 signed 做逻辑右移
-  unsigned int d = (unsigned)b;
-  printf("d = 0x%x, sizeof(d) = %lu\n", d, sizeof(d));
-  printf("d>>2 = 0x%x, sizeof(d >> 2) = %lu\n", d >> 2, sizeof(d >> 2));
+  unsigned int d = (unsigned)a;
+  printf("d = 0x%x, d = %u, sizeof(d) = %lu\n", d, d, sizeof(d));
+  printf("d>>2 = 0x%x, d>>2 = %u, sizeof(d >> 2) = %lu\n", d >> 2, d >> 2,
+         sizeof(d >> 2));
 
   return 0;
 }
