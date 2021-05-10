18dc62f01643ad3c89ad2849d3d15e5363dd2837
nilinli <nilinli@tuhu.cn>
2021-05-10 10:43:49 +0800

    [javascript] ts enum

--- javascript/es-next/typescript/basic/src/basic-type/enum.ts
+++ javascript/es-next/typescript/basic/src/basic-type/enum.ts
@@ -1,7 +1,9 @@
 // enum (枚举) -> 使用枚举我们可以定义一些带名字的常量
 
-// 1 数字枚举
-// 自增长， 0 开始
+// 数字枚举
+// 自增长
+// 增长基于上一个 enum 值
+// 第一个值默认为 0 开始
 enum Direction1 {
     Up,
     Down,
@@ -11,26 +13,7 @@ enum Direction1 {
 
 console.log(Direction1.Down); // 1
 
-enum Direction2 {
-    Up = 1,
-    Down,
-    Left,
-    Right
-};
-
-console.log(Direction2.Down); // 2
-
-enum Direction3 {
-    Up = 4,
-    Down,
-    Left = 1000,
-    Right 
-}
-
-console.log(Direction3.Down); // 5
-console.log(Direction3.Right); // 1001
-
-// 2 字符串
+// 字符串
 enum Direction21 {
     Up = 'up',
     Down = 'down',
@@ -39,3 +22,58 @@ enum Direction21 {
 }
 
 console.log(Direction21.Down);
+
+// 异构 不推荐这样做
+enum BooleanLikeHeterogeneous {
+    No = 0,
+    Yes = 'Yes'
+};
+
+// 枚举值定义
+enum FileAcess {
+    // 常量 编译时候求值 ts 表达式子集
+    None,           // 0
+    A,              // 1
+    B = 2008,
+    C,              // 2009
+    Read = 1 << 1,  // 2
+    Write = 1 << 2, // 4
+    ReadWrite = Read | Write,   // 6
+    // 运行时候求值
+    G = '123'.length        // '123'.length
+}
+
+// 枚举在运行时候真实存在, 不是编译时候的代码替换, 和 macro 不同
+// var E;
+// (function (E) {
+//     E[E["X"] = 0] = "X";
+//     E[E["Y"] = 1] = "Y";
+//     E[E["Z"] = 2] = "Z";
+// })(E || (E = {}));
+enum E {
+    X, Y, Z
+};
+
+function funcEnum(obj: any) {
+    console.log(obj);
+}
+
+funcEnum(E);
+
+// 生成的代码包含正向映射和反向映射
+console.log(E[2]);
+
+// 常量枚举 macro 相同, 编译时候删除
+const enum M {
+    X, Y = X + 2, Z
+};
+
+const mArr = [M.X, M.Y, M.Z];
+
+// 外部枚举
+// 没有初始化方法时候, 当作被计算的而不是常量
+declare enum ExtenalEnum {
+    A = 1,
+    B,
+    C = 2
+}
--- pf-node/networking/websocket/origin/ws/index.js
+++ pf-node/networking/websocket/origin/ws/index.js
@@ -2,6 +2,14 @@ const EventEmitter = require('events');
 const crypto = require('crypto');
 const getSPHeaders = require('./getSPHeaders');
 
+const opcodes = {
+  TEXT: 1,
+  BINARY: 2,
+  CLOSE: 8,
+  PING: 9,
+  PONG: 10
+};
+
 class Websocket extends EventEmitter {
   constructor(req, socket, upgradeHead) {
     super();
@@ -9,7 +17,6 @@ class Websocket extends EventEmitter {
     // 升级协议
     socket.write(getSPHeaders(req));
 
-    
     socket.on('data', (data) => {
       // this.buffer = Buffer.concat([this.buffer, data]);
       // while (this._processBuffer()) {} // 循环处理返回的 data 数据
@@ -22,7 +29,7 @@ class Websocket extends EventEmitter {
       // }
     });
 
-    // 3. 初始化成员属性
+
   }
 }
 
