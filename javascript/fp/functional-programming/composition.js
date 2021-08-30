c2a74f9798672df494e2ca8bc8134a855b443b45
linli.ni <linli.ni@yuanquan.com>
2021-08-30 18:18:45 +0800

    [javascript] fp compose

--- javascript/fp/functional-programming/composition.js
+++ javascript/fp/functional-programming/composition.js
@@ -1,10 +1,27 @@
 // 函数组合 composition
-// The act of putting 
-// two functions together to form a third function where the output of one function is the input of the other.
-// 从右向左
-const compose = (f, g) => (a) => f(g(a));
+// 将一系列函数组合执行
 
-const floorAndToString = compose(val => val.toString(), Math.floor);
-const result = floorAndToString(121.111111);
+// 从左到右注册
+// 从右向左执行 pipe 相反
+// 先执行的结果当作后执行的参数
 
-console.log(result);
+function compose(...funcs) {
+  // 提供一个初始参数
+  // 返回一个函数
+  return function composedFunc(initArgs) {
+    let args = initArgs;
+    for (let i = funcs.length - 1; i >= 0; i--) {
+      const func = funcs[i];
+      args = func(args);
+    }
+    return args;
+  };
+}
+
+
+
+// const compose = (f, g, h) => (a) =>  f(g(h(a)));
+
+const floorAndToString = compose((val) => val.toString(), Math.floor);
+
+console.log(floorAndToString(121.111111));
