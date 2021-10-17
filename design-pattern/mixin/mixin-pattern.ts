0aba5a0ec84ea5596540896350e243c4a8f0bb23
nilinli <nilinli1994@gmail.com>
2021-10-17 21:36:50 +0800

    [design-pattern] js mixin pattern

--- design-pattern/mixin/mixin-pattern.ts
+++ design-pattern/mixin/mixin-pattern.ts
@@ -1,18 +1,11 @@
-// 继承有 prototype 的各种方式
-// 但是 已经存在的对象 继承 不适合 prototype 的方式
-
-// mixin pattern 就是解决这个问题
-// 为任意数量的对象(普通对象, prototype)实例添加任意多个属性
-
-// Object.assign 代替了 自定义的 extend
-
-export { };
-
+// 已有对象
 let target = {
     name: 'nixixi',
     age: 18
 };
-let result = Object.assign(target,
+
+let newTarget = Object.assign({},
+    target,
     {
         city: 'wuhan'
     },
@@ -21,7 +14,6 @@ let result = Object.assign(target,
     }
 );
 
-console.log(target === result); // true
-console.log(result);
+console.log(newTarget);
 
 
