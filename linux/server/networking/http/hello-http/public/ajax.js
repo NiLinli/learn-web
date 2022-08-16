ee467dbb9fdcfcc64f012cdf7eb3ce965a65e6a9
nilinli <nilinli1994@gmail.com>
2022-08-16 22:13:24 +0800

    update http put method

--- linux/server/networking/http/hello-http/public/ajax.js
+++ linux/server/networking/http/hello-http/public/ajax.js
@@ -1,44 +1,38 @@
 function ajax({ url, method = 'get', headers = {}, params = {}, body = {}, cb }) {
   return new Promise((resolve, reject) => {
-    let headersTemp = {}
+    let headersTemp = {};
 
     if (['get', 'delete'].includes(method)) {
       // 无默认 Content-Type
-    } else if (['post', 'put'].includes(method)) {
-      headersTemp['Content-Type'] = 'application/json'
-    } else {
-      headersTemp['Content-Type'] = 'application/json'
+    } else if (['post'].includes(method)) {
+      headersTemp['Content-Type'] = 'application/json';
     }
 
     headersTemp = {
       ...headersTemp,
       ...headers,
-    }
+    };
 
-    var xhr = new XMLHttpRequest()
-
-
-
-
+    var xhr = new XMLHttpRequest();
 
     // true 为 async
     // false 有些浏览器已经不支持
-    const queryString = formatSearchParams(params)
-    xhr.open(method, `${url}${queryString ? '?' + queryString : ''}`, true)
+    const queryString = formatSearchParams(params);
+    xhr.open(method, `${url}${queryString ? '?' + queryString : ''}`, true);
 
     for (const k in headersTemp) {
       if (Object.hasOwnProperty.call(headersTemp, k)) {
-        xhr.setRequestHeader(k, headersTemp[k])
+        xhr.setRequestHeader(k, headersTemp[k]);
       }
     }
 
     cb && cb(xhr);
 
-    if (['post', 'put'].includes(method)) {
+    if (['post'].includes(method)) {
       if (headersTemp['Content-Type'] === 'application/json') {
-        xhr.send(JSON.stringify(body))
+        xhr.send(JSON.stringify(body));
       } else if (headersTemp['Content-Type'] === 'application/x-www-form-urlencoded') {
-        xhr.send(formatSearchParams(body))
+        xhr.send(formatSearchParams(body));
       } else if (headersTemp['Content-Type'] === 'multipart/form-data') {
         const formData = new FormData();
         for (const k in body) {
@@ -46,12 +40,14 @@ function ajax({ url, method = 'get', headers = {}, params = {}, body = {}, cb })
             formData.append(k, body[k]);
           }
         }
-      
-        xhr.send(formData)
+
+        xhr.send(formData);
       }
+    } else if (method === 'put') {
+      xhr.send(body);
     } else {
       // 没有请求体
-      xhr.send()
+      xhr.send();
     }
 
     // 返回结果
@@ -59,7 +55,6 @@ function ajax({ url, method = 'get', headers = {}, params = {}, body = {}, cb })
       // If the request is compete and was successful
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-
           if (xhr.responseType === 'json') {
             return resolve(xhr.response);
           }
@@ -69,30 +64,30 @@ function ajax({ url, method = 'get', headers = {}, params = {}, body = {}, cb })
           }
 
           // responseType 为 'text' '' 读取 responseText
-          const type = xhr.getResponseHeader('Content-Type')
+          const type = xhr.getResponseHeader('Content-Type');
           if (type.indexOf('application/json') !== -1) {
-            resolve(JSON.parse(xhr.responseText))
+            resolve(JSON.parse(xhr.responseText));
           } else {
-            resolve(xhr.responseText)
+            resolve(xhr.responseText);
           }
         } else {
           reject();
         }
-      } 
-    })
-  })
+      }
+    });
+  });
 }
 
 function formatSearchParams(data) {
-  if (!data) return ''
-  var pairs = []
+  if (!data) return '';
+  var pairs = [];
   for (var name in data) {
-    if (!data.hasOwnProperty(name)) continue
-    if (typeof data[name] === 'function') continue
-    var value = data[name].toString()
-    name = encodeURIComponent(name.replace(' ', '+')) // Encode name
-    value = encodeURIComponent(value.replace(' ', '+')) // Encode value
-    pairs.push(name + '=' + value) // Remember name=value pair
+    if (!data.hasOwnProperty(name)) continue;
+    if (typeof data[name] === 'function') continue;
+    var value = data[name].toString();
+    name = encodeURIComponent(name.replace(' ', '+')); // Encode name
+    value = encodeURIComponent(value.replace(' ', '+')); // Encode value
+    pairs.push(name + '=' + value); // Remember name=value pair
   }
-  return pairs.join('&')
+  return pairs.join('&');
 }
--- linux/server/networking/http/hello-http/routes/file.js
+++ linux/server/networking/http/hello-http/routes/file.js
@@ -4,15 +4,24 @@ const fs = require('fs');
 const path = require('path');
 
 router.post('/uploadImg', (req, res, next) => {
+  // form-data 参数 body-parser 不处理
+  res.setHeader('x-location','123')
   res.send({
-    data: 'Omg six'
-  })
+    data: 'Omg six',
+  });
+})
+
+router.put('/uploadImg', (req, res, next) => {
+  // console.log(1111, req)
+  res.setHeader('x-location','123')
+  res.end('234 d')
 })
 
 router.post('/getSomeFile', (req, res, next) => {
   const rs = fs.createReadStream(path.resolve(__dirname, '../public/ajax.js'))
   rs.pipe(res);
 })
+
 
 
 module.exports = router;
