9e484ddae49b3a6548e51696d98f455672752d8a
joeni <joeni@tencent.com>
2023-02-03 14:40:44 +0800

    update shell demo

---
+++ linux/shell/demo/git-migrate.sh
@@ -0,0 +1,30 @@
+# "CoralMall" "CoralGMall2"  "CoralPay" "CoralInventory" "CoralLottery"
+# for i in "CoralMall" "CoralPay" "CoralInventory" 
+# do
+#   cd /Users/nilinli/omg/
+#   git clone --bare "git@git.code.tencent.com:jmdhy/$i.git"
+#   cd "./$i.git"
+#   git push --mirror "git@git.tencent.com:jmdhy/standard/$i.git"
+#   cd /Users/nilinli/omg/
+# done
+
+
+# "CoralTimer" "CoralLotteryScfServer" "CoralPurchase"
+# for i in "CoralPurchase"
+# do
+#   cd /Users/nilinli/omg/
+#   git clone --bare "git@git.woa.com:CSIG_Tsec_P2_MSPC/Coral/$i.git"
+#   cd "./$i.git"
+#   git push --mirror "git@git.tencent.com:jmdhy/standard/$i.git"
+#   cd /Users/nilinli/omg/
+# done
+
+
+# for i in "CoralMall" "CoralGMall2"  "CoralPay" "CoralInventory" "CoralLottery" "CoralTimer" "CoralLotteryScfServer" "CoralPurchase"
+# do
+#   cd /Users/nilinli/omg/
+#   git clone --bare "git@git.tencent.com:jmdhy/standard/$i.git"
+#   cd "./$i.git"
+#   git push --mirror "git@git.tencent.com:jmdhy/standard/node/$i.git"
+#   cd /Users/nilinli/omg/
+# done
