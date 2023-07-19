902bd2d2e52ed8a716965d06ae5dec1d6baf0117
joeni <joeni@tencent.com>
2023-07-19 14:52:07 +0800

    [micro-mall] ...

--- be-framework/micro-mall/mall-order/src/main/java/com/example/mall/order/OrderApplication.java
+++ be-framework/micro-mall/mall-order/src/main/java/com/example/mall/order/OrderApplication.java
@@ -4,13 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = { "com.example.mall" })
 @MapperScan(basePackages = "com.example.mall.order.dao")
 @EnableCaching
-@EnableRedisHttpSession
+@EnableFeignClients
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- be-framework/micro-mall/mall-order/src/main/java/com/example/mall/order/feign/ProductFeignClient.java
+++ be-framework/micro-mall/mall-order/src/main/java/com/example/mall/order/feign/ProductFeignClient.java
@@ -1,12 +1,15 @@
 package com.example.mall.order.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.mall.product.model.pojo.Product;
 import com.example.mall.product.model.request.ProductUpdateStockReq;
 
+@FeignClient(value = "mall-product")
 public interface ProductFeignClient {
+  
   @PostMapping("/inner/product/detailForFeign")
   Product detailForFeign(@RequestBody Integer id);
 
